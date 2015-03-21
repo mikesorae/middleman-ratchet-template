dc1201c378a98e03c5617c546bdf70e4bb96db46
mikesorae <i.love.poko247@gmail.com>
2015-03-22 00:25:09 +0900

    add helper for badges

--- lib/ui_helper.rb
+++ lib/ui_helper.rb
@@ -10,10 +10,18 @@ class UIHelper < Middleman::Extension
       btn += " btn-outlined" if outline
       "<a class='#{btn}' href='#{url}'>#{text}</a>"
     end
+
     def ui_switch(on: false)
       toggle = "toggle"
       toggle += " active" if on
       "<div class='#{toggle}'><div class='toggle-handle'></div></div>"
+    end
+
+    def ui_badge(number, type: "", invert: false)
+      badge = "badge"
+      badge += " badge-#{type}" if type.present?
+      badge += " badge-inverted" if invert
+      "<span class='#{badge}'>#{number}</span>"
     end
   end
 end
