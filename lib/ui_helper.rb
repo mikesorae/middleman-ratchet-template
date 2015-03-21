a85bfa754721fa2bfd1c5d99cf60c17ff40390af
mikesorae <i.love.poko247@gmail.com>
2015-03-22 00:14:51 +0900

    remove "btn-" prefix from type argment

--- lib/ui_helper.rb
+++ lib/ui_helper.rb
@@ -6,7 +6,7 @@ class UIHelper < Middleman::Extension
   helpers do
     def ui_button(text, url="", type: "", outline: false)
       btn = "btn"
-      btn += " #{type}" if type.present?
+      btn += " btn-#{type}" if type.present?
       btn += " btn-outlined" if outline
       "<a class='#{btn}' href='#{url}'>#{text}</a>"
     end
